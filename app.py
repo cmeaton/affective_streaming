f5b9ca3ea2f294cf4bdd22320e94613cd0575a46
Keyan Pishdadian <kpishdadian@gmail.com>
2019-03-21 23:59:46 -0700

    It's alive

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, Response, request
+import base64
 
-from streaming import convert_img_bytes_to_numpy_array, convert_numpy_array_to_img, process_frame
+from flask import Flask, make_response, request
+
+from streaming import (
+    convert_img_bytes_to_numpy_array,
+    convert_numpy_array_to_img,
+    process_frame,
+)
 
 app = Flask(__name__)
 
@@ -8,13 +14,13 @@ app = Flask(__name__)
 @app.route('/process', methods=['POST', 'OPTIONS'])
 def process():
     if request.method == 'OPTIONS':
-        resp = Response()
+        resp = make_response()
     else:
         frame = convert_img_bytes_to_numpy_array(request.files['webcam'].read())
-        resp_data = convert_numpy_array_to_img(process_frame(frame))
-        resp = Response(resp_data)
+        byte_data = convert_numpy_array_to_img(process_frame(frame))
+        decoded_data = base64.b64encode(byte_data).decode('utf-8')
+        resp = make_response(decoded_data)
         resp.headers['Content-Type'] = 'text/plain'
-        # resp.headers['Content-Disposition'] = 'attachment'
 
     resp.headers['Access-Control-Allow-Origin'] = "*"
     resp.headers['Access-Control-Allow-Headers'] = "*"
